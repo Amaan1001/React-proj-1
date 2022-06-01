b75374685378bc4e84e4500e18e71d35e4f583a5
Amaan1001 <amaan.faruk@gmail.com>
2022-06-01 10:13:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+
+require('./config/passport')(passport);
+
+const db = require('./config/keys').MongoURI;
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+//template engine
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//body parser middleware
+app.use(express.urlencoded({ extended:true}));
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+//routes
+app.use('/', require('./routes/index.js'));
+app.use('/users',require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
+
